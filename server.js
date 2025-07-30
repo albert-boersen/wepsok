c50526beb84f02c2b7309c80adc9ea58b3c15d31
Albert Boersen <boerdt.dongen@gmail.com>
2025-07-30 15:52:49 +0200

    change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ wss.on('connection', ws => {
     ws.send(`Echo: ${msg}`);
     wss.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(msg);
       }
     });
   });
